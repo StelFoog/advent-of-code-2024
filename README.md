af5af7ab13633a448ad4b48f9234d5e043df0a4d
Samuel Larsson <samuel@familjenlarsson.eu>
2024-12-11 09:20:32 +0100

    Day 10

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ It's also possible to use example inputs by including `true` after the task to b
 - [x] Day 7
 - [x] Day 8
 - [x] Day 9
-- [ ] Day 10
+- [x] Day 10
 - [ ] Day 11
 - [ ] Day 12
 - [ ] Day 13
---
+++ tasks/10-1.ts
@@ -0,0 +1,24 @@
+import { input } from "../src/inputManager";
+
+const map = input.split("\n").map((line) => line.split("").map(Number));
+
+let sum = 0;
+for (let y = 0; y < map.length; y++) {
+	for (let x = 0; x < map[y].length; x++) if (map[y][x] === 0) sum += findTrails(x, y);
+}
+console.log(sum);
+
+function findTrails(x: number, y: number) {
+	const ends = new Set<string>();
+	let queue = [{ x, y }];
+	while (queue.length) {
+		const { x, y } = queue.shift()!;
+		const height = map[y][x];
+		if (height === 9) ends.add(`${x}|${y}`);
+		if (map[y - 1]?.[x] === height + 1) queue.push({ x, y: y - 1 });
+		if (map[y + 1]?.[x] === height + 1) queue.push({ x, y: y + 1 });
+		if (map[y][x - 1] === height + 1) queue.push({ x: x - 1, y });
+		if (map[y][x + 1] === height + 1) queue.push({ x: x + 1, y });
+	}
+	return ends.size;
+}
